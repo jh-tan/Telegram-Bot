32f6791c6beb91f8ad55069a0343366ff82c41a4
jh-tan <hui971105@gmail.com>
2021-06-20 17:31:29 +0800

    Added procfile

--- index.js
+++ index.js
@@ -18,19 +18,20 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 let getMatch = null
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(config.API_TOKEN);
 
 const options = {
-  webHook: {
-    // Port to which you should bind is assigned to $PORT variable
-    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
-    port: process.env.PORT
-    // you do NOT need to set up certificates since Heroku provides
-    // the SSL certs already (https://<app-name>.herokuapp.com)
-    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
-  }
+	webHook: {
+		// Port to which you should bind is assigned to $PORT variable
+		// See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+		port: process.env.PORT
+		// you do NOT need to set up certificates since Heroku provides
+		// the SSL certs already (https://<app-name>.herokuapp.com)
+		// Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
+	}
 };
 const url = process.env.APP_URL || 'https://ukm-uni-chat.herokuapp.com:443';
+const port = process.env.PORT;
+const bot = new TelegramBot(config.API_TOKEN,options);
 
 bot.setWebHook(`${url}/bot${config.API_TOKEN}`);
 
@@ -167,4 +168,13 @@ const matchUser = async (msg) =>{
 }
 
 bot.on('polling_error', (err) => console.log(err))
+
+
+fastify.listen(port, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`server listening on ${address}`)
+})
 
